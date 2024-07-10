3e22f7f0773bd83582d8e0a66c9ba62ee8355ba1
Noah Clark <noahammon00@gmail.com>
2024-07-10 11:08:01 -0700

    initial commit: genned app and added basic api

---
+++ src/test/kotlin/com/okta/kotlin/resourceserver/ReactKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.okta.kotlin.resourceserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
